f1d788cf495511747e9000a056051c1a350543f7
Luis <lmr.87@protonmail.ch>
2022-01-27 12:14:25 +0100

    13.- Creada la clase Consola

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/Consola.java
@@ -1,10 +1,12 @@
 package org.iesalandalus.programacion.reservasaulas.mvc.vista;
 
-import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Aula;
-import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Permanencia;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.*;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
 
 public class Consola {
     private static final DateTimeFormatter FORMATO_DIA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -39,9 +41,59 @@ public class Consola {
     }
 
     public static Aula leerAula() {
-        System.out.printf("\nIntroduzca el nombre de el nuevo aula: ");
+        return new Aula(leerNombreAula());
+    }
 
-        return new Aula(Entrada.cadena());
+    /** No se exactamente que tiene que hacer este método */
+    public static String leerNombreAula() {
+        System.out.print("\nIntroduzca el nombre de el nuevo aula: ");
+
+        return Entrada.cadena();
+    }
+
+    public static Profesor leerProfesor() {
+        String nombre = leerNombreProfesor();
+
+        System.out.print("\nIntroduzca el correo de el nuevo profesor: ");
+        String correo = Entrada.cadena();
+
+        System.out.print("\nIntroduzca el telefono de el nuevo profesor (Opcional): ");
+        String telefono = Entrada.cadena();
+
+        if (telefono.trim().isEmpty())
+            return new Profesor(nombre, correo);
+
+        return new Profesor(nombre, correo, telefono);
+    }
+
+    public static String leerNombreProfesor() {
+        System.out.print("\nIntroduzca el nombre de el nuevo profesor: ");
+        return Entrada.cadena();
+    }
+
+    public Tramo leerTramo() {
+        String tramoString = "";
+        do {
+            System.out.print("\nIntroduzca el tramo de la reserva (mañana/tarde): ");
+            tramoString = Entrada.cadena();
+        } while (!Objects.equals(tramoString, "mañana") && !Objects.equals(tramoString, "tarde"));
+
+        if (tramoString == "mañana")
+            return Tramo.MANANA;
+
+        return Tramo.TARDE;
+    }
+
+    public static LocalDate leerDia() {
+        System.out.printf("%s (dd/MM/yyyy): ", "Introduzca la fecha ");
+        String fechaStr = Entrada.cadena();
+        LocalDate fecha = null;
+        try {
+            fecha = LocalDate.parse(fechaStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("ERROR: La fecha introducida no tiene el formato adecuado.");
+        }
+        return fecha;
     }
 
 
